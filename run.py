4090d1ee01a16faee705e9dcaacc48cb14e47702
Ferdosull <ferdia.osullivan@gmail.com>
2021-04-15 19:04:55 +0000

    Flask, providing feedback to the user through advanced routing

--- run.py
+++ run.py
@@ -3,7 +3,16 @@ import os
 # Import the json Library
 import json
 # Importing the Flask class from flask, pulling in a template
-from flask import Flask, render_template
+# import the 'request' library from Flask. Request is going to
+# handle things like finding out what method we used,
+# and it will also contain our form object when we've posted it
+# Importing flasj to display some feedback to the user
+from flask import Flask, render_template, request, flash
+
+# Import env if the system can find an env.py file.
+
+if os.path.exists("env.py"):
+    import env
 
 
 # !!Two blank lines between code blocks & functions to keep PEP8 compliant!!
@@ -11,6 +20,7 @@ from flask import Flask, render_template
 
 # Creating an instance of the Flask class and saving it in the variable "app"
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")  # Decorator
@@ -26,8 +36,22 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")  # Decorator
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])  # Decorator
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
